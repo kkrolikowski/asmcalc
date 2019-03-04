a92d192eee8a7b6af365519dce335b54329f9c1f
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-03-04 22:45:34 +0100

    basic info

---
+++ main.asm
@@ -0,0 +1,10 @@
+;                       Asm calculator
+;                      =================
+; Calculator accepts three args. Second arg should be one of the valid
+; operators: + - / *
+; As the result of division calculator gives number with remainder.
+;
+; Examples: 
+;   ./asmcalc 1 + 1
+;   ./asmcalc 2 * 2
+;   ./asmcalc 4 - 1
