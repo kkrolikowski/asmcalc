6ec55f21fbbbd0a4a18d087b64420bdafb384ef4
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-03-13 00:59:27 +0100

    toInt() implementation

--- main.asm
+++ main.asm
@@ -30,3 +30,30 @@ tooFewError         db "Error: Too few arguments.", LF
                     db "Syntax: ./asmcalc a [+|-|*|/] b", LF, NULL
 tooManyError        db "Error: Too many arguments.", LF
                     db "Syntax: ./asmcalc a [+|-|*|/] b", LF, NULL
+
+; -----
+; Calculation results
+
+sum                 dq 0
+
+section .bss
+numA                resq 1
+numB                resq 1
+
+extern toInt
+
+section .text
+
+global main
+main:
+    mov r12, rdi                        ; argc
+    mov r13, rsi                        ; *argv[]
+
+    mov rdi, qword [r13+1*8]
+    call toInt
+    mov qword [numA], rax
+
+last:
+    mov rax, sys_EXIT
+    mov rdi, EXIT_SUCCESS
+    syscall
