e04f0efb2973e358b07587fb541eeeffc2b0601a
Krzysztof Królikowski <kkrolikowski@gmail.com>
2019-03-30 10:09:08 +0100

    comments

--- math.asm
+++ math.asm
@@ -25,10 +25,13 @@ global calc
 calc:
     push rbp
     mov rbp, rsp
-    sub rsp, 21
+    sub rsp, 21                         ; local string array char ar[21]
     push rbx
 
-    lea rbx, byte [rbp-21]
+    lea rbx, byte [rbp-21]              ; array pointer
+
+; -----
+; capture valid operator
 
     cmp sil, "+"
     je calcSum
@@ -41,16 +44,25 @@ calc:
 
     jmp calcEnd
 
+; -------------------------------------------------------------------------------
+;                                   Multiplication
+
 calcMul:
     mov rax, rdi
     imul rdx
 
-    cmp rdx, 0
+    cmp rdx, 0                          ; check if result fits in 64 bits.
     ja ResTooBig
+
+; -----
+; convert result to string
 
     mov rdi, rax
     mov rsi, rbx
     call int2str
+
+; -----
+; display result with following newline
 
     mov rdi, rbx
     call prints
@@ -58,6 +70,9 @@ calcMul:
     call prints
     
     jmp calcEnd
+
+; -----
+; If result is over 64 bit - display an error
 
 ResTooBig:
     mov rdi, toobig
@@ -65,16 +80,25 @@ ResTooBig:
 
     jmp calcEnd
 
+; -------------------------------------------------------------------------------
+;                                   Sum
+
 calcSum:
     mov rax, rdi
     add rax, rdx
 
-    cmp rax, 0
-    je printZero
+    cmp rax, 0                          ; if result is 0, display 0 as string
+    je printZero                        ; without further conversion
+
+; -----
+; convert result to string
 
     mov rdi, rax
     mov rsi, rbx
     call int2str
+
+; -----
+; Display result with following newline
 
     mov rdi, rbx
     call prints
@@ -82,17 +106,26 @@ calcSum:
     call prints
 
     jmp calcEnd
+
+; -------------------------------------------------------------------------------
+;                                   Subtraction
 
 calcSub:
     mov rax, rdi
     sub rax, rdx
     
-    cmp rax, 0
-    je printZero
+    cmp rax, 0                          ; if result is 0, display 0 as string
+    je printZero                        ; without further conversion
+
+; -----
+; convert result to string
 
     mov rdi, rax
     mov rsi, rbx
     call int2str
+
+; -----
+; Display result with following newline
 
     mov rdi, rbx
     call prints
@@ -100,6 +133,9 @@ calcSub:
     call prints
 
     jmp calcEnd
+
+; -------------------------------------------------------------------------------
+;                                   Division
 
 calcDiv:
     mov rax, rdi
@@ -107,18 +143,30 @@ calcDiv:
     cqo
     idiv r10
 
+; -----
+; convert result to string
+
     mov rdi, rax
     mov rsi, rbx
     call int2str
+
+; -----
+; Display result with following remainder prefix
 
     mov rdi, rbx
     call prints
     mov rdi, remainder
     call prints
 
+; -----
+; convert remainder to string
+
     mov rdi, rdx
     mov rsi, rbx
     call int2str
+
+; -----
+; Display remainder with following newline
 
     mov rdi, rbx
     call prints
